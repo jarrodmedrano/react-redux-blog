6f53a4354f0784125548c1adf8325ee402b80fb5
jmedran <jmedran@gmail.com>
2016-03-04 17:11:36 -0800

    adding errors

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,28 +1,33 @@
 import React, { Component } from 'react'
 import { reduxForm } from 'redux-form'
+import { createPost } from '../actions/index';
 
 
 class PostsNew extends Component {
   render() {
     const { fields: { title, categories, content }, handleSubmit } = this.props;
 
-    console.log(title);
     //const title = this.props.fields.title
 
     return (
-      <form onSubmit={ handleSubmit }>
+      <form onSubmit={ handleSubmit(this.props.createPost) }>
         <h3>Create a New Post</h3>
         <div className="form-group">
           <label>Title</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" {...title} />
+          <div className="text-help">{title.touched ? title.error : ''}
           </div>
+        </div>
         <div className="form-group">
           <label>Categories</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" {...categories} /> <div className="text-help">{categories.touched ? categories.error : ''}
+        </div>
         </div>
         <div className="form-group">
           <label>Content</label>
-          <textarea className="form-control" />
+          <textarea className="form-control" {...content} />
+          <div className="text-help">{content.touched ? content.error : ''}
+          </div>
         </div>
 
         <button type="submit" className="btn btn-primary">Submit</button>
@@ -31,7 +36,26 @@ class PostsNew extends Component {
   }
 }
 
+
+function validate(values) {
+  const errors = {};
+  if(!values.title) {
+    errors.title = 'Enter a username';
+  }
+  if(!values.categories) {
+    errors.categories = 'Enter a category';
+  }
+  if(!values.content) {
+    errors.content = 'Enter content';
+  }
+
+  return errors;
+}
+// connect: first argument is mapStateToProps, 2nd is mapDispatchToProps
+
+//reduxForm 1st is form config, 2nd is mapstatetoprops, 3rd is mapdispatchtoprops
 export default reduxForm({
   form: 'PostsNewForm',
-  fields: ['title', 'categories', 'content']
-})(PostsNew);
+  fields: ['title', 'categories', 'content'],
+  validate
+}, null, { createPost } )(PostsNew);
